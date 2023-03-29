b7680d8c7eafb9b4293f1d586f868048d6660e57
Warren Wice <mulubiwarren@gmail.com>
2023-03-29 22:41:02 +0300

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,6 @@
-ewrewr
+import {RowElement,RowID } from './interface';
+
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
